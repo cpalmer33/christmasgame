248c6fa46e829fa91f53f824ee9ccb72bfda215c
prudepixie <wndyhsu@gmail.com>
2020-12-13 17:06:06 -0800

    fixed loader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Slide from './Slide'
 import SlideTwo from './SlideTwo'
 import SlideThree from './SlideThree'
@@ -15,18 +15,18 @@ function App() {
 
     return (
         <>
-        <div className="app">
-        <Router>
-            <Switch>
-            <Route path="/" exact component={LandingPage}/>
-            <Route path="/slide" exact component={Slide}/>
-            <Route path="/slidetwo" exact component={SlideTwo}/>
-            <Route path="/slidethree" exact component={SlideThree}/>
-            <Route path="/slidefour" exact component={SlideFour}/>
-            <Route path="/slidefive" exact component={SlideFive}/>
-            </Switch>
-        </Router>
-        </div>
+            <div className="app">
+                <Router>
+                    <Switch>
+                        <Route path="/" exact component={LandingPage} />
+                        <Route path="/slide" exact component={Slide} />
+                        <Route path="/slidetwo" exact component={SlideTwo} />
+                        <Route path="/slidethree" exact component={SlideThree} />
+                        <Route path="/slidefour" exact component={SlideFour} />
+                        <Route path="/slidefive" exact component={SlideFive} />
+                    </Switch>
+                </Router>
+            </div>
         </>
     )
 }
